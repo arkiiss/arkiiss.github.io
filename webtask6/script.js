842bb77461d87a11c36d2021c1971678ecef8b0e
arkiiss <157961214+arkiiss@users.noreply.github.com>
2024-10-19 19:31:56 +0300

    Add files via upload

---
+++ webtask6/script.js
@@ -0,0 +1,109 @@
+
+
+function updateprice()
+{
+    let s=document.getElementsByName("product");
+    let select=s[0];
+    let prices=getprice();
+    let count=document.getElementsByName("count");
+    let price=prices.pType[(select.value)-1]*count[0].value;
+
+    let OpDisplay=document.getElementById("options");
+    let PrDisplay=document.getElementById("checkboxes");
+    let checkboxes=document.querySelectorAll("#checkboxes input");
+    let radios=document.getElementsByName("rad2");
+
+    if(select.value==1)
+    {    OpDisplay.style.display="none";
+        PrDisplay.style.display="none";
+        checkboxes.forEach(function(checkbox){
+            checkbox.checked=0;
+        })
+            radios[0].checked=1;
+        
+    }
+    if(select.value==2)
+    {
+        OpDisplay.style.display="flex";
+        PrDisplay.style.display="none";
+        checkboxes.forEach(function(checkbox){
+            checkbox.checked=0;
+        })
+    }
+    if(select.value==3)
+        {
+            OpDisplay.style.display="none";
+            PrDisplay.style.display="flex";
+            radios[0].checked=1;
+        }
+    
+    let delivery = document.getElementsByName("rad1");
+    delivery.forEach(function(radio){
+        if(radio.checked && prices.pDelivery[radio.value-1]!==undefined)
+            price+=prices.pDelivery[radio.value-1];
+    })
+    let options=document.getElementsByName("rad2");
+    options.forEach(function(radio){
+        if(radio.checked && prices.pOption[radio.value-1]!==undefined)
+            price+=prices.pOption[radio.value-1]*count[0].value;
+    })
+    let property=document.querySelectorAll("#checkboxes input");
+    property.forEach(function(checkbox){
+        if(checkbox.checked && prices.pProp[checkbox.value-1]!==undefined)
+            price+=prices.pProp[checkbox.value-1]*count[0].value;
+    })
+    let res=document.getElementById("result");
+    if(isNaN(price))
+    {alert("Введите число");}
+    else
+    {res.innerHTML=price+" рублей";}
+    
+}
+function getprice()
+{
+    return {pType:[20000,25000, 65000],
+        pDelivery:[499, 99, 0],
+        pOption:[0, 4000],
+        pProp:[5000,10000]
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function(event)
+{
+    let c=document.getElementsByName("count");
+    let count=c[0];
+    count.addEventListener("change", function(event)
+{
+    updateprice();
+})
+    
+
+
+
+    let s=document.getElementsByName("product");
+    let select=s[0];
+    select.addEventListener("change", function(event){
+        updateprice();
+    })
+
+    let delivery = document.getElementsByName("rad1");
+    delivery.forEach(function(radio){
+        radio.addEventListener("change",function(event)
+    {
+        updateprice();
+    })})
+    let options=document.getElementsByName("rad2");
+    options.forEach(function(radio){
+        radio.addEventListener("change", function(event){
+            updateprice();
+        })
+    })
+    let property=document.querySelectorAll("#checkboxes input");
+    property.forEach(function(checkbox){
+        checkbox.addEventListener("change", function(event){
+            updateprice();
+        })
+    })
+
+    updateprice();
+})
