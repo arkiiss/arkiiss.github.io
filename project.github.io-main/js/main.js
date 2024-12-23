ecfeb55962aff58578120240663928a6a8967566
masyuncheg <maksim.lebedev2005@mail.ru>
2024-12-23 11:38:08 +0300

    Update main.js

--- project.github.io-main/js/main.js
+++ project.github.io-main/js/main.js
@@ -83,4 +83,60 @@ $(function() {
     })
 })
 
+$(function(){
+            $("#form").submit(function(e){
+                e.preventDefault();
 
+                let name = document.getElementById('edit-field-vashe-imya-0-value').value;
+                let number = document.getElementById('edit-field-telefon-0-value').value;
+                let email = document.getElementById('edit-field-e-mail-0-value').value;
+                let comment = document.getElementById('edit-field-vash-0-value').value;
+                let check = document.getElementById('edit-fz152-agreement').checked;
+        
+                if (!name || !email || number=='+7' || !number || !comment || !check) {
+                    alert('Заполните все поля!!!');
+                    return;
+                }
+        
+                localStorage.setItem('name', name);
+                localStorage.setItem('email', email);
+                localStorage.setItem('number', number);
+                localStorage.setItem('comment', comment);
+            
+        
+            
+              var href = $(this).attr("action");
+              
+              $.ajax({
+                  type: "POST",
+                  url: href,
+                  data: new FormData(this),
+                  dataType: "json",
+                  processData: false,
+                  contentType: false,
+                  success: function(response){
+                    if(response.status == "success"){
+                        alert("Мы получили вашу заявку, спасибо вам");
+                        localStorage.clear();
+                        form.reset();
+                    }
+                    else if(response.code === 422){
+                      alert("Field validation failed");
+                      $.each(response.errors, function(key) {
+                        $('[name="' + key + '"]').addClass('formcarry-field-error');
+                      });
+                    }
+                    else{
+                      alert("An error occured: " + response.message);
+                    }
+                  },
+                  error: function(jqXHR, textStatus){
+                    const errorObject = jqXHR.responseJSON
+          
+                    alert("Request failed, " + errorObject.title + ": " + errorObject.message);
+                  }
+              });
+            });
+          });
+    
+});
